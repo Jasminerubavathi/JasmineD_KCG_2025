13c76625b6e9761985fc824e532dd8bdef61ba5d
Jasmine D _KCG_2025 <96763869+Jasminerubavathi@users.noreply.github.com>
2024-01-23 23:51:21 +0530

    Add files via upload
    21.01.24_qno 3

---
+++ 23.01.24_your name.c
@@ -0,0 +1,62 @@
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+# Hello firstname lastname! You just delved into python.
+
+# Function Description
+
+ # Complete the print_full_name function in the editor below.
+
+ # print_full_name has the following parameters:
+
+# string first: the first name
+ # String last: the last name
+ # Prints
+
+ # string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
+ # Input Format
+
+ # The first line contains the first name, and the second line contains the last name.
+
+  # Constraints
+
+ # The length of the first and last names are each ≤ .
+
+# Sample Input 0
+
+ #Ross
+# Taylor
+# Sample Output 0
+
+# Hello Ross Taylor! You just delved into python.
+# explanation 0
+
+#The input read by the program is stored as a string data type. A string is a collection of characters.
+
+#Contest ends in 18 hours
+#Submissions: 85
+#Max Score: 10
+# Difficulty: Easy
+ # Rate This Challenge:
+
+    
+More
+ 
+1
+#
+2
+# Complete the 'print_full_name' function below.
+3
+#
+4
+# The function is expected to return a STRING.
+5
+# The function accepts following parameters:
+6
+#  1. STRING first
+7
+#  2. STRING last
+8
+
+print(f"Hello {first} {last}! You just delved into python.")
+
+​
